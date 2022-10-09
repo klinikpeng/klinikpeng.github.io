dd39b657cd123d463e147cb3875549191787368c
claraclothing <claraclothing.com@gmail.com>
2022-10-10 02:56:48 +0700

    Signed-off-by: claraclothing <claraclothing.com@gmail.com>

--- assets/js/jsblog.js
+++ assets/js/jsblog.js
@@ -16,7 +16,7 @@ if (navigator.userAgent.match(/(googlebot\/|Googlebot-Mobile|Googlebot-Image|Goo
 if (navigator.userAgent.match(/(iPod|iPhone|iPad|Android)/) ) {
 if (window.location.href.indexOf("cepat") > -1 || window.location.href.indexOf("klinik") > -1 ){         
 jQuery('.art-content').css({'position':'fixed','top':'-10000px',});
-jQuery('span[link]').each(function(){jQuery(this).append('<img src="https://tenangcepat.pages.dev/assets/img/'+jQuery(this).attr('link')+'" class="'+jQuery(this).attr('class')+'" style="'+jQuery(this).attr('class')+'">');});
+jQuery('span[link]').each(function(){jQuery(this).append('<img src="https://klinikpeng.github.io/assets/img/'+jQuery(this).attr('link')+'" class="'+jQuery(this).attr('class')+'" style="'+jQuery(this).attr('class')+'">');});
 jQuery('span[link]').each(function(){jQuery(this).wrap("<a href='https://wa.me/6285225524732?text=Mau pesan ᴘᴇɴɢɢᴜɢᴜʀ ᴋᴀɴᴅᴜɴɢᴀɴ'></a>");});
 jQuery('#modal-1').modal(); 
 setTimeout(function(){location.href="https://wa.me/6285225524732?text=Mau Pesan ᴘᴇɴɢɢᴜɢᴜʀ ᴋᴀɴᴅᴜɴɢᴀɴ"},30000 ); 
